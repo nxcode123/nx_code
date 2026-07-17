ef67fd31c3af1c69b4ee121cbb4dd5f430f25556
NX-Code <nexoncode7@gmail.com>
2026-07-17 17:51:15 +0700

    fork

--- src/menu/submenu/ubuntu_menu.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# ==============================================================================
-# SUBMENU UBUNTU (untuk ekspansi nanti)
-# ==============================================================================
-
-show_ubuntu_submenu() {
-    # Untuk nanti jika ada banyak opsi Ubuntu
-    ubuntu_cli
-}
